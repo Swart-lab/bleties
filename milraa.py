16260655e2562b0666d45121d11e5566742d528a
Kwee Boon Brandon Seah <kb.seah@tuebingen.mpg.de>
2020-03-27 04:10:49 +0100

    Count left- and right-clipped reads as breakpoint evidence
     * Add to attributes field of GFF3 output, like MIRAA
     * However the primary evidence is from insertion operation,
       because in long reads we expect most of the putative IESs to be
       spanned by the read

--- milraa.py
+++ milraa.py
@@ -42,10 +42,9 @@ parser.add_argument("--max_mismatch", # TODO: Not yet implemented
                     help="Maximum mismatch in the alignment for a read to be used")
 args = parser.parse_args()
 
+# Check that only either SAM or BAM specified
 aln_filename = "-"
 aln_mode = "r"
-
-# Check that only either SAM or BAM specified
 if args.sam:
     if args.bam:
         sys.exit("Error: Specify either SAM or BAM input, not both")
@@ -59,23 +58,36 @@ if args.bam:
         aln_filename=args.bam
         aln_mode = "rb"
 
-# dict to store counts of detected inserts
-# keys: contig -> startpos -> ies_length -> count
+# dict to store counts of detected inserts keyed by evidence type
+# keys: contig -> startpos -> evidence type -> count
 insDict = defaultdict(lambda: defaultdict( lambda: defaultdict (int)))
+# dcit to store counts of detected inserts keyed by insert length
+# keys: contig -> startpos -> ies_length -> count
+insLenDict = defaultdict(lambda: defaultdict( lambda: defaultdict (int)))
 
 # Open SAM file and parse CIGAR string
 alnfile = pysam.AlignmentFile(aln_filename, aln_mode)
 for line in alnfile:
     # linearr = line.split("\t") # Split line by tabs
-    pos = int(line.reference_start) + 1 # Convert from 0-based numbering in pysam to 1-based
-    rname = line.reference_name
-    total_mismatch = line.get_tag("NM")
+    pos = int(line.reference_start) + 1 # Convert from 0-based numbering in pysam to 1-based in GFF3 and SAM
+    rname = line.reference_name # Get reference name
+    total_mismatch = line.get_tag("NM") # Get number of mismatches
     cigs = re.findall(r"\d+[\w\=]", line.cigarstring) # Get CIGAR string
     # Initialize counters for how much query or reference seq is consumed
     # TODO: What about hard clipping?
     ref_consumed = 0
     que_consumed = 0
     total_i = 0 
+    # Look for inserts that are on left or right ends of read (i.e. H or S clipping operations)
+    leftclipmatch = re.match(r"(\d+)[HS](\d+)M", line.cigarstring)
+    rightclipmatch = re.search(r"(\d+)M(\d+)[HS]$", line.cigarstring)
+    if leftclipmatch:
+        insDict[rname][pos]['HSM'] += 1
+    if rightclipmatch:
+        refconsumematches = re.findall(r"(\d+)[MDN\=X]", line.cigarstring) # find all ref-consuming operations
+        rightclip_refconsumed = sum([int(refconsumematch) for refconsumematch in refconsumematches])
+        insDict[rname][pos + rightclip_refconsumed]['MHS'] += 1
+    # Look for inserts that are completely spanned by the read (i.e. I operations)
     for cig in cigs: # for each cig in the string
         cigmatch = re.match(r"(\d+)([\w\=])",cig) # Get number and operation
         # We want to look for insert operations above a min length, 
@@ -89,7 +101,8 @@ for line in alnfile:
             if ins_len >= args.min_ies_length: # Check that insert is above min length
                 # Get the start and end positions of the insert
                 ins_pos_start = pos + ref_consumed # 1-based, insert is to the right of position
-                insDict[rname][ins_pos_start][ins_len] += 2 # write to dict; counts as 2 because each insert has two ends
+                insLenDict[rname][ins_pos_start][ins_len] += 1 # record the putative IES length spanned by the read
+                insDict[rname][ins_pos_start]["I"] += 2 # record as "I" type count, like MIRAA; counts as 2 because each insert has two ends
         # Count ref and query consumed _after_ the insert has been accounted for
         if cigmatch.group(2) in ['M', 'D', 'N', '=', 'X']:
             ref_consumed += int(cigmatch.group(1))
@@ -102,14 +115,18 @@ for line in alnfile:
 # print(json.dumps(insDict, sort_keys=True, indent = 2))
 
 # Parse the dict and report putative IESs above min coverage
-for ctg in sorted(insDict):
-    for ins_start in sorted(insDict[ctg]):
-        for ins_len in sorted(insDict[ctg][ins_start]):
-            if insDict[ctg][ins_start][ins_len] >= args.min_break_coverage:
+# We only check breakpoints which are completely spanned by a read ("I" type)
+# however we also report supporting counts from HSM and MSH type mappings
+for ctg in sorted(insLenDict):
+    for ins_start in sorted(insLenDict[ctg]):
+        for ins_len in sorted(insLenDict[ctg][ins_start]):
+            if insLenDict[ctg][ins_start][ins_len] >= args.min_break_coverage:
                 # Prepare attributes list of key-value pairs
                 attr = ["ID=BREAK_POINTS_"+str(ctg)+"_"+str(ins_start),
                         "IES_length="+str(ins_len)
-                       ] # TODO: cigar
+                       ]
+                # Extend the cigar evidence counts
+                attr.extend(["cigar="+cigartype+" "+str(insDict[ctg][ins_start][cigartype]) for cigartype in sorted(insDict[ctg][ins_start])])
                 # Get read coverage from BAM file; SAM does not allow random access
                 if aln_mode == "rb":
                     readcov = alnfile.count(str(ctg), start=int(ins_start)-1, stop=int(ins_start))
@@ -119,7 +136,7 @@ for ctg in sorted(insDict):
                           "segment",       # 3 type
                           str(ins_start),  # 4 start
                           str(ins_start),  # 5 end
-                          str(insDict[ctg][ins_start][ins_len]), # 6 score - in this case, breakpoint counts
+                          str(insLenDict[ctg][ins_start][ins_len]), # 6 score - in this case, breakpoint counts
                           ".",             # 7 strand
                           ".",             # 8 phase
                           ";".join(attr)   # 9 attributes
