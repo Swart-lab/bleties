cdd2a9fceb3d59efc3f99aa6bf6aa93992661d9a
Kwee Boon Brandon Seah <kb.seah@tuebingen.mpg.de>
2020-04-07 12:29:49 +0200

    Tweak docs for Milraa

--- milraa.py
+++ milraa.py
@@ -1,16 +1,5 @@
 #!/usr/bin/env python3
 
-"""MILRAA - Method of Identification by Long Read Alignment Anomalies
-The MIRAA module in ParTIES uses an alignment of Illumina reads vs somatic 
-genome to look for breakpoints in read alignment. This script reimplements the 
-MIRAA workflow for PacBio or other long read alignments.
-
-Differences to Illumina alignments:
- * One read may have multiple inserts
- * Reads are not paired, insert size is not an issue
- * Error rate of reads is expected to be higher
-"""
-
 import argparse
 import sys
 import pysam
@@ -18,7 +7,17 @@ from Bio import SeqIO
 from Bio.SeqRecord import SeqRecord
 from bleties import *
 
-parser = argparse.ArgumentParser(description="MILRAA - MIRAA equivalent for long reads mappings, e.g. PacBio",
+parser = argparse.ArgumentParser(description=
+"""MILRAA - Method of Identification by Long Read Alignment Anomalies
+The MIRAA module in ParTIES uses an alignment of Illumina reads vs somatic 
+genome to look for breakpoints in read alignment. This script reimplements the 
+MIRAA workflow for PacBio or other long read alignments.
+
+Differences to Illumina alignments:
+* One read may have multiple inserts
+* Reads are not paired, insert size is not an issue
+* Error rate of reads is expected to be higher
+""",
                                  formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument("--sam",
                     help="SAM file containing mapping, requires header")
