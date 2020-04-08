f1c318b4c1a0dd53bcdd8bb420354e60c867703a
Kwee Boon Brandon Seah <kb.seah@tuebingen.mpg.de>
2020-04-08 04:48:36 +0200

    Add function to find what alignment operation is at given ref position

--- bleties/IesRecords.py
+++ bleties/IesRecords.py
@@ -10,6 +10,7 @@ from Bio.Seq import Seq
 from Bio.SeqRecord import SeqRecord
 from Bio.Align import MultipleSeqAlignment
 from Bio.Align import AlignInfo
+from bleties.SharedValues import SharedValues
 
 def getClips(cigar, pos):
     """Parse cigar string and alignment position and report left- and right-
@@ -83,7 +84,7 @@ def getIndels(cigar, pos, minlength, qseq):
             que_consumed += int(cigmatch.group(1))
     return(outarr)
 
-def isMatchAtRefPos(reftargetpos, refstartpos, cigar):
+def isMatchAtRefPos(reftargetpos, refstartpos, cigar, mininslength, minmatchlength):
     """Identify whether a given reference position is covered by a reference-
     consuming operation. 
     Returns the operation that covers that position. If no operation covers
@@ -93,6 +94,8 @@ def isMatchAtRefPos(reftargetpos, refstartpos, cigar):
     reftargetpos -- Target position on the reference sequence, 1-based (int)
     refstartpos -- Start position of the alignment on reference, 1-based (int)
     cigar -- CIGAR string of the alignment (str)
+    mininslength -- Minimum length of insert to report (int)
+    minmatchlength - Minimum length of match to report (int)
     """
     
     # Lists of reference- and query-consuming operations
@@ -103,13 +106,27 @@ def isMatchAtRefPos(reftargetpos, refstartpos, cigar):
     cigs = re.findall(r"\d+[\w\=]", cigar)
     for cig in cigs:
         cigmatch = re.match(r"(\d+)([\w\=])",cig) # Get number and operation
+        # If reference is consumed:
         if cigmatch.group(2) in REFCONSUMING:
             # Update the current interval
             curr_int_start = curr_int_end
             curr_int_end = curr_int_end + int(cigmatch.group(1))
+        # Otherwise if query is consumed
+        else:
+            curr_int_start = curr_int_end # current operation has extent zero on ref
+
+        # If current operation is ref-consuming
+        if curr_int_end > curr_int_start:
             # Check whether the target position is contained in the current interval
-        if reftargetpos in range(curr_int_start,curr_int_end):
-            return(cigmatch.group(2))
+            if reftargetpos in range(curr_int_start,curr_int_end): # TODO check off-by-one errors
+                if int(cigmatch.group(1)) > minmatchlength:
+                    return(cigmatch.group(2))
+        # If current operation interval is zero (i.e. not ref-consuming)
+        elif curr_int_end == curr_int_start:
+            # and it matches exactly the target poosition
+            if reftargetpos == curr_int_end:
+                if int(cigmatch.group(1)) > mininslength:
+                    return(cigmatch.group(2))
 
 class IesRecords(object):
     """Records of putative IESs from mappings"""
