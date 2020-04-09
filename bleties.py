e3bd073576b197d90bf2f4297682011190aacd96
Kwee Boon Brandon Seah <kb.seah@tuebingen.mpg.de>
2020-04-09 05:29:35 +0200

    Notes on todos

--- bleties.py
+++ bleties.py
@@ -17,6 +17,15 @@ parser.add_argument("--log",
 subparsers = parser.add_subparsers()
 
 # MILRAA -----------------------------------------------------------------------
+"""The MIRAA module in ParTIES uses an alignment of Illumina reads vs somatic 
+genome to look for breakpoints in read alignment. This script reimplements the 
+MIRAA workflow for PacBio or other long read alignments.
+
+Differences to Illumina alignments:
+* One read may have multiple inserts
+* Reads are not paired, insert size is not an issue
+* Error rate of reads is expected to be higher
+"""
 milraa_parser = subparsers.add_parser(name="milraa",
                                       description="MILRAA - Method of Identification by Long Read Alignment Anomalies")
 milraa_parser.add_argument("--sam",
@@ -56,6 +65,30 @@ milraa_parser.add_argument("--dump",
 milraa_parser.set_defaults(func=main.milraa) 
 
 # MILRET -----------------------------------------------------------------------
+"""The MIRET pipeline in ParTIES compares mappings of the same reads to the 
+somatic and germline genomes, at known IES junctions. Reads that map with match
+to the somatic version are counted as IES-, reads that map with match to the 
+germline version are counted as IES+. 
+
+With long reads, we assume that IESs are spanned completely by most reads, vs
+short reads, where reads are unlikely to completely span an IES insert. So we 
+do not count soft/hard clips on the ends of reads, only matches and inserts. We
+also assume that the read mapper will handle mapping of reads containing inserts
+properly. Therefore, we only map the reads to the somatic genome. Reads that do
+contain IES+ forms will be reported as mappings with insert operations ("I" in 
+the CIGAR string). We then simply compare reads mapping with match to the 
+somatic genome at the IES junction (counted as IES-) to reads mapping with an 
+insert at the exact location of the IES junction (counted as IES+).
+
+Points to note and address in the future:
+ * Long reads are noisier and have many small indels. How do we distinguish 
+   sequencing error from true inserts?
+ * For same reason as above: What is minimum match length/quality before we 
+   count a match?
+ * Some junctions also exhibit deletions, which may be alternative excisions,
+   misassembly, or misalignments. 
+"""
+
 milret_parser = subparsers.add_parser(name="milret",
                                       description="""MILRET - Method of IES Long-read RETention""")
 milret_parser.add_argument("--bam",
@@ -77,13 +110,3 @@ milret_parser.set_defaults(func=main.milret)
 args = parser.parse_args()
 # Execute respective functions for each subparser
 args.func(args)
-
-"""The MIRAA module in ParTIES uses an alignment of Illumina reads vs somatic 
-genome to look for breakpoints in read alignment. This script reimplements the 
-MIRAA workflow for PacBio or other long read alignments.
-
-Differences to Illumina alignments:
-* One read may have multiple inserts
-* Reads are not paired, insert size is not an issue
-* Error rate of reads is expected to be higher
-"""
