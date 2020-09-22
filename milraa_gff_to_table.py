1575fa93a9255d228864baea029e0b2a84597fb8
Kwee Boon Brandon Seah <kb.seah@tuebingen.mpg.de>
2020-09-23 00:11:24 +0200

    Plot IES stats

--- milraa_gff_to_table.py
+++ milraa_gff_to_table.py
@@ -4,8 +4,14 @@ from bleties import SharedFunctions
 from sys import argv
 import re
 import json
+import pandas as pd
+
+import matplotlib as mpl
+mpl.use("Agg") # allow run without X-server
+import matplotlib.pyplot as plt
 
 gff_file = argv[1]
+out_prefix = argv[2]
 
 gff_obj = SharedFunctions.Gff()
 gff_obj.file2gff(gff_file)
@@ -43,4 +49,40 @@ for gff_id in gff_obj:
             row['pointer'] = "none"
     out.append(row)
 
-print(json.dumps(out, indent=4))
+with open(f"{out_prefix}.json", "w") as fh:
+    fh.write(json.dumps(out, indent=4))
+
+# Plotting
+df = pd.DataFrame.from_dict(out)
+
+# IES retention score histogram
+plt.figure()
+plt.hist([df.query('type == "ins"')['retention_score'],
+          df.query('type == "del"')['retention_score']],
+         label = ['ins','del'], stacked=True, bins=20)
+plt.legend()
+plt.xlabel("IES retention score")
+plt.ylabel("Number of putative IESs")
+plt.savefig(f"{out_prefix}.retention_score.png")
+
+# IES lengths and pointer types
+plt.figure()
+plt.hist([df.query("pointer == 'none'")["length"],
+          df.query("pointer == 'ta'")["length"],
+          df.query("pointer == 'pointer'")["length"]],
+         stacked=True, bins=100, label=['none','ta','pointer'])
+plt.legend()
+plt.xlabel("Length (bp)")
+plt.ylabel("Number of putative IESs")
+plt.savefig(f"{out_prefix}.ies_length.png")
+
+# IES lengths and pointer types - closeup
+plt.figure()
+plt.hist([df.query("length > 25 & length <= 400 & pointer == 'none'")["length"],
+          df.query("length > 25 & length <= 400 & pointer == 'ta'")["length"],
+          df.query("length > 25 & length <= 400 & pointer == 'pointer'")["length"]],
+         stacked=True, bins=375, label=['none','ta','pointer'])
+plt.legend()
+plt.xlabel("Length (bp)")
+plt.ylabel("Number of putative IESs")
+plt.savefig(f"{out_prefix}.ies_length_closeup.png")
