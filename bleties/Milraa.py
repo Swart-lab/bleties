4ca62abb7d6fe7356cc49d569ed96b9722a0978d
Kwee Boon Brandon Seah <kb.seah@tuebingen.mpg.de>
2020-09-25 19:24:35 +0200

    Skip supplementary mappings in MILRAA, use pysam to parse bitflags

--- bleties/Milraa.py
+++ bleties/Milraa.py
@@ -665,20 +665,19 @@ class IesRecords(object):
         """
         # parse CIGAR string
         for line in self._alnfile:
-            if not line.flag & 4: # Skip unmapped reads
-                if not line.flag & 256: # Skip secondary mappings
-                    pos = int(line.reference_start) + 1 # Convert from 0-based numbering in pysam to 1-based in GFF3 and SAM
-                    rname = line.reference_name # Get reference name
-                    # total_mismatch = line.get_tag("NM") # Get number of mismatches
-                    # total_i = 0
-                    qseq = line.query_sequence # Get query sequence
-                    # Find left and right clips and record them
-                    self._addClipsFromCigar(rname, line.cigarstring, pos)
-                    # Find indels (putative IESs) over the minimum length and record them
-                    self._addIndelsFromCigar(rname, line.cigarstring, pos, minlength, qseq)
-                    # # if int(total_mismatch) - int(total_i) < 0:
-                    #     # Sanity check - mismatches include inserts, but cannot be fewer than inserts
-                    #     # print ("Uh-oh!")
+            if (not line.is_unmapped) and (not line.is_secondary) and (not line.is_supplementary):
+                pos = int(line.reference_start) + 1 # Convert from 0-based numbering in pysam to 1-based in GFF3 and SAM
+                rname = line.reference_name # Get reference name
+                # total_mismatch = line.get_tag("NM") # Get number of mismatches
+                # total_i = 0
+                qseq = line.query_sequence # Get query sequence
+                # Find left and right clips and record them
+                self._addClipsFromCigar(rname, line.cigarstring, pos)
+                # Find indels (putative IESs) over the minimum length and record them
+                self._addIndelsFromCigar(rname, line.cigarstring, pos, minlength, qseq)
+                # # if int(total_mismatch) - int(total_i) < 0:
+                #     # Sanity check - mismatches include inserts, but cannot be fewer than inserts
+                #     # print ("Uh-oh!")
 
         # Go through insDict and extract sequences of deleted regions, too
         self._getDeletedSequences()
